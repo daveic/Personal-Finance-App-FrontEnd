44687afb74830c90a5230ca09ba09bd7f88b69a5
daveic <88085267+daveic@users.noreply.github.com>
2022-01-14 09:50:28 +0100

    add

--- Models/Balances.cs
+++ Models/Balances.cs
@@ -4,6 +4,7 @@ namespace PersonalFinanceFrontEnd.Models
 {
     public class Balance
     {
+        public string Usr_OID { get; set; }
         public int ID { get; set; }
         public DateTime BalDateTime { get; set; }
         public int ActBalance { get; set; }
