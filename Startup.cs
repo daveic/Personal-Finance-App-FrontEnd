5efef14d228126038100226785b1a8f034225b37
daveic <88085267+daveic@users.noreply.github.com>
2022-02-11 10:51:08 +0100

    2

--- Startup.cs
+++ Startup.cs
@@ -45,12 +45,10 @@ namespace PersonalFinanceFrontEnd
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
