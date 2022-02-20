bb1a8c255c9059b7c520744ff3ebbc80922a5119
Matteo Tumiati <setumiatim@tetrapak.com>
2022-02-20 15:55:46 +0100

    ok

--- Controllers/PersonalFinanceController.cs
+++ Controllers/PersonalFinanceController.cs
@@ -905,11 +905,25 @@ namespace PersonalFinanceFrontEnd.Controllers
         }
         //DEBITS Intermediate view
         [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
-        public ActionResult Expirations()
+        public ActionResult Expirations(string selectedYear)
         {
             string User_OID = GetUserData().Result; //Fetch User Data
-            IEnumerable<Expiration> Expirations = GetAllItems<Expiration>(nameof(Expirations), User_OID).OrderBy(item => item.ExpDateTime.Month);
-            ViewBag.Expirations = Expirations.Take(5).ToList(); //Fetch imminent expirations
+            IEnumerable<Expiration> Expirations = GetAllItems<Expiration>(nameof(Expirations), User_OID);
+            ViewBag.Expirations = Expirations.Where(x => x.ExpDateTime.Year.ToString() == DateTime.Now.Year.ToString()).OrderBy(item => item.ExpDateTime.Month).Take(5).ToList(); //Fetch imminent expirations
+            //Trovo gli anni "unici"
+            var UniqueYear = Expirations.GroupBy(item => item.ExpDateTime.Year)
+                    .Select(group => group.First())
+                    .Select(item => item.ExpDateTime.Year)
+                    .ToList();
+            //Creo la lista di anni "unici" per il dropdown filter del grafico saldo
+            List<SelectListItem> itemlistYear = new List<SelectListItem>();
+            foreach (var year in UniqueYear.Skip(1)) itemlistYear.Add(new SelectListItem() { Text = year.ToString(), Value = year.ToString() });
+            //Passo alla view la lista
+            ViewBag.ItemList = itemlistYear;
+            //Se al caricamento della pagina ho selezionato un anno (not empty), salvo in Balances i saldi di quell'anno
+            if (!String.IsNullOrEmpty(selectedYear)) Expirations = Expirations.AsQueryable().Where(x => x.ExpDateTime.Year.ToString() == selectedYear).OrderBy(item => item.ExpDateTime.Month);
+            else Expirations = Expirations.AsQueryable().Where(x => x.ExpDateTime.Year.ToString() == DateTime.Now.Year.ToString()).OrderBy(item => item.ExpDateTime.Month);
+
 
             ViewModel viewModel = new ViewModel();
             viewModel.Expiration = new Expiration();
