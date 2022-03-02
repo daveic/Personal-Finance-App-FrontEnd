3b2609ae1ae80115fcba471af8460bed7ca99f29
Davide Alessi <dade.10@outlook.com>
2022-03-02 15:11:59 +0100

    new adds

---
+++ Models/Budget_Calc.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace PersonalFinanceFrontEnd.Models
+{
+    public class Ticket
+    {
+        public string Usr_OID { get; set; }
+        public int ID { get; set; }
+        public string TicketName { get; set; }
+        public string NumTicket { get; set; }
+        public double TicketValue { get; set; }
+        public string TicketNote { get; set; }
+        public string input_value { get; set; }
+    }
+}
