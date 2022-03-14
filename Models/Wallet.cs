5edbed9410a57f3bd31150c455a56373fdcf7316
daveic <88085267+daveic@users.noreply.github.com>
2022-03-14 17:56:18 +0100

    add

---
+++ Models/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+
+namespace PersonalFinanceFrontEnd.Models
+{
+    public class Wallet
+    {
+        public IEnumerable<Bank> Banks { get; set; }
+        public Bank Bank { get; set; }
+        public IEnumerable<Deposit> Deposits { get; set; }
+        public Deposit Deposit { get; set; }
+        public IEnumerable<Ticket> Tickets { get; set; }
+        public Ticket Ticket { get; set; }
+        public Bank Contanti { get; set; }
+    }
+}
